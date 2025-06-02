8ded373b65b6482c3972c26e5df77d1e7f0e9add
ivan <ivanmarti.alves@gmail.com>
2025-06-02 09:58:10 -0300

    fix atulizei as paginas do projeto

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <h1 class="text-center mt-8">Hello World</h1>
+
+    <main class="flex-1">
+      <HelloWorld/> 
+      <TheWelcome />
+    </main>
+  </div>
+</template>
+<style scoped>
+h1 {
+  color: red;
+  font-size: 2rem;
+}
+</style>
